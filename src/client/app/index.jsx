c681f6b334620ee67576cfc300610d64a0fcb0f9
Rob Weber <robertweber95@gmail.com>
2017-04-12 17:48:49 -0400

    Add browserHistory support for GitHub Pages

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+
+const Home = () => <h1>Hello from Home!</h1>
+const About = () => <h1>We are located at 555 Jackson St.</h1>
 
 class App extends Component {
   render() {
-    return <p> Hello React!</p>
+    <Router history={hashHistory}>
+      <Route path='/' component={Home} />
+      <Route path='/about' component={About} />
+    </Router>
   }
 }
 
