d53536b4723a62118e9349330a0e85b6300fcbe4
Rob Weber <robertweber95@gmail.com>
2017-04-12 18:49:08 -0400

    Add simple routing

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Container from './components/container'
 
 const Home = () => <h1>Hello from Home!</h1>
 const About = () => <h1>We are located at 555 Jackson St.</h1>
 
 class App extends Component {
   render() {
-    <Router history={hashHistory}>
-      <Route path='/' component={Home} />
-      <Route path='/about' component={About} />
-    </Router>
+    return (
+      <Router>
+        <div>
+          <Container >
+            <Route exact path='/' component={Home} >
+            </Route>
+            <Route path='/about' component={About}>
+            </Route>
+          </Container>
+        </div>
+      </Router>
+    )
   }
 }
 
